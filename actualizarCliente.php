7389efeaf718b491c507809f9314c0fbaeea80db
Alejandro Hermitaño <jesus.hy.96@gmail.com>
2020-12-19 02:36:06 -0500

    Editar Cliente a un 50% ya captura falta hacer consulta

---
+++ actualizarCliente.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sedapal | Editar Cliente</title>
+    <link rel="icon" type="icon/png" href="img/icono_sedapal.png">
+    <link rel="stylesheet" href="css/principal.css">
+</head>
+<body>
+    <!-- AQUI EMPIEZA EL MENU -->
+    <header>
+        <section class="icono_principal">
+            <img src="img/logo.png" alt="logo principal">
+        </section>
+        <section class="menu">
+                <ul class="nav">
+                    <li>
+                        <a href="">Registrar</a>
+                        <ul>
+                            <li><a href="registrarMedidor.php">Medidor</a></li>
+                            <li><a href="registrarCliente.php">Cliente</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Editar</a>
+                        <ul>
+                            <li><a href="#">Medidor</a></li>
+                            <li><a href="editarCliente.php">Cliente</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Buscar</a>
+                        <ul>
+                            <li><a href="#">Medidor</a></li>
+                            <li><a href="buscarCliente.php">Cliente</a></li>
+                        </ul>
+                    </li>
+                </ul>
+        </section>
+    </header>
+
+    <!-- FORMULARIO DE BUSQUEDA -->
+    <div>
+        <h2 class="tituloBuscar">Cliente a Editar</h2>
+    </div>
+
+    <!-- AQUI EMPIEZA EL CODIGO PHP -->
+    <?php
+        // Estp es para buscar al cliente
+        $inc=include("database.php");
+        $id2 = $_GET["id"];
+        $consulta = "SELECT * FROM cliente WHERE idcliente LIKE '$id2'";
+        $resultado = mysqli_query($conex, $consulta);
+        while ($row = mysqli_fetch_array($resultado)) { ?>
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Tipo Doc</th>
+                        <th>Numero Doc</th>
+                        <th>Tipo Persona</th>
+                        <th>Pais</th>
+                        <th>Departamento</th>
+                        <th>Distrito</th>
+                        <th>Direccion</th>
+                    </tr>  
+                    <tr>
+                        <th><input type="text" value="<?php echo $row['idcliente']; ?>"></th>
+                        <td><input type="text" value="<?php echo $row['nombre']; ?>"></td>
+                        <td><input type="text" value="<?php echo $row['apellidos']; ?>"></td>
+                        <td><select name="TipoDocumento" id="TipoDocumento">
+                            <option value="DNI">DNI</option>
+                            <option value="Carne de Extranjeria">Carne de Extranjeria</option>
+                        </select></td>
+                        <td><input type="text" value="<?php echo $row['numDoc']; ?>"></td>
+                        <td><select name="TipoPersona" id="TipoPersona">
+                            <option value="Natural">Natural</option>
+                            <option value="Jurídica">Jurídica</option>
+                        </select></td>
+                        <td><select name="paisProcedencia" id="paisProcedencia">
+                            <option value="Peru">Perú</option>
+                            <option value="Colombia">Colombia</option>
+                            <option value="Argentina">Argentina</option>
+                            <option value="Venezuela">Venezuela</option>
+                            <option value="Brasil">Brasil</option>
+                            <option value="Bolivia">Bolivia</option>
+                        </select></td>
+                        <td><select name="departamento" id="departamento">
+                            <option value="Lima">Lima</option>
+                            <option value="Callao">Callao</option>
+                        </select></td>
+                        <td>
+                            <select name="distrito" id="distrito">
+                                <option value="Lima">Callao</option>
+                                <option value="Callao">Bellavista</option>
+                                <option value="Callao">Carmen de la Legua</option>
+                                <option value="Callao">La Perla</option>
+                                <option value="Callao">La Punta</option>
+                                <option value="Callao">Ventanilla</option>
+                                <option value="Callao">Mi Perú</option>
+                                <option value="Callao">Ancon</option>
+                                <option value="Callao">Ate Vitarte</option>
+                                <option value="Callao">Barranca</option>
+                                <option value="Callao">Breña</option>
+                                <option value="Callao">Carabayllo</option>
+                                <option value="Callao">Chaclacayo</option>
+                                <option value="Callao">Chorillos</option>
+                                <option value="Callao">Cieneguilla</option>
+                                <option value="Callao">Comas</option>
+                                <option value="Callao">El Agustino</option>
+                            </select>
+                        </td>
+                        <td><input type="text" value="<?php echo $row['direccion']; ?>"></td>
+                    </tr>
+                </tbody>
+            </table>
+            <button type="submit" name="registrar" id="registrar">Modificar</button>
+        <?php    
+        }
+    ?>
+    <!-- AQUI EMPIEZA EL FOOTER -->
+    <div class="footer">
+        <div class="redes">
+            <a href="#"><img src="img/fb.png" alt="facebook"></a>
+            <a href="#"><img src="img/twitter.png" alt=""></a>
+            <a href="#"><img src="img/youtube.png" alt=""></a>
+        </div>
+        <p>Derechos Reservados | Sedapal &copy;</p>
+        <p>AquaFono: 3271859</p>
+    </div>
+
+</body>
+</html>
--- editarCliente.php
+++ editarCliente.php
@@ -38,7 +38,74 @@
         </section>
     </header>
     <!-- AQUI TERMINAR EL MENU -->
-    
+
+    <!-- FORMULARIO DE BUSQUEDA -->
+    <form action="editarCliente.php" method="POST">
+        <input type="text" name="numDoc" placeholder="Consultar DNI">
+        <input type="submit" name="buscar" id="buscar">
+    </form>
+
+    <!-- AQUI EMPIEZA EL CODIGO PHP -->
+    <?php
+        // Estp es para buscar al cliente
+        $inc=include("database.php");
+        if (isset($_POST['buscar'])) {
+            $numDoc = $_POST['numDoc'];
+            $consulta = "SELECT * FROM cliente WHERE numDoc LIKE '$numDoc'";
+            $resultado = mysqli_query($conex, $consulta);
+            if ($resultado) {
+                while ($row = $resultado->fetch_array()) {
+                    $id = $row['idcliente'];
+                    $nombre = $row['nombre'];
+                    $apellidos = $row['apellidos'];
+                    $tipoDoc = $row['tipoDoc'];
+                    $numDoc = $row['numDoc'];
+                    $tipoPersona = $row['tipoPersona'];
+                    $pais = $row['pais'];
+                    $departamento = $row['departamento'];
+                    $distrito = $row['distrito'];
+                    $direccion = $row['direccion'];
+                    ?>
+                    <div>
+                         <h2 class="tituloBuscar">Lista de Clientes</h2>
+                         <table border="1">
+                             <tbody>
+                                 <tr>
+                                     <th>ID</th>
+                                     <th>Nombre</th>
+                                     <th>Apellidos</th>
+                                     <th>Tipo Doc</th>
+                                     <th>Numero Doc</th>
+                                     <th>Tipo Persona</th>
+                                     <th>Pais</th>
+                                     <th>Departamento</th>
+                                     <th>Distrito</th>
+                                     <th>Hola</th>
+                                     <th>ACCION</th>
+                                 </tr>
+                                 <tr>
+                                     <th><?php echo $id ?></th>
+                                     <td><?php echo $nombre ?></td>
+                                     <td><?php echo $apellidos ?></td>
+                                     <td><?php echo $tipoDoc ?></td>
+                                     <td><?php echo $numDoc ?></td>
+                                     <td><?php echo $tipoPersona ?></td>
+                                     <td><?php echo $pais ?></td>
+                                     <td><?php echo $departamento ?></td>
+                                     <td><?php echo $distrito ?></td>
+                                     <td><?php echo $direccion ?></td>
+                                     <td><a href="actualizarCliente.php?id=<?php echo $row['idcliente'];?>"><img src="img/editar.png" alt=""></a></td>
+                                 </tr>
+                             </tbody>
+                         </table>
+                    </div>
+                    <?php     
+                }
+            }
+        }
+
+    ?>
+
     <!-- AQUI EMPIEZA EL FOOTER -->
     <div class="footer">
         <div class="redes">
